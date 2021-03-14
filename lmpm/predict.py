0c302545be4c8cb029731de3580742d65876afd7
marcexpositg <marcexpositg@users.noreply.github.com>
2021-03-14 21:10:59 +0100

    predictions return class and renamed function

--- lmpm/predict.py
+++ lmpm/predict.py
@@ -35,7 +35,9 @@ for i, row in params.iterrows():
 model_human = pickle.load(open(os.path.join(current_wd,"models","human.pkl"), 'rb'))
 model_yeast = pickle.load(open(os.path.join(current_wd,"models","yeast.pkl"), 'rb'))
 model_ecoli = pickle.load(open(os.path.join(current_wd,"models","ecoli.pkl"), 'rb'))
-
+model_human_dg = pickle.load(open(os.path.join(current_wd,"models","human_dg.pkl"), 'rb'))
+model_yeast_dg = pickle.load(open(os.path.join(current_wd,"models","yeast_dg.pkl"), 'rb'))
+model_ecoli_dg = pickle.load(open(os.path.join(current_wd,"models","ecoli_dg.pkl"), 'rb'))
 
 
 
@@ -56,9 +58,9 @@ def model_organism_selection(organism, include_dg):
     # Check specification of additional features
     if include_dg:
         model_dict = {
-            'human': model_human,
-            'yeast': model_yeast,
-            'ecoli': model_ecoli
+            'human': model_human_dg,
+            'yeast': model_yeast_dg,
+            'ecoli': model_ecoli_dg,
         }
     else: 
         model_dict = {
@@ -72,58 +74,6 @@ def model_organism_selection(organism, include_dg):
 
     return model
 
-
-def localization_score(input_seq, organism, target_class='secreted', include_dg=False):
-    """
-    Finds the probability of being the target class given the
-    sequence based on model and UniRep representation only
-
-    Args:
-        input_seq (str): sequence entered by the user
-        organism (str):  specific organism of sequence type 
-                         (all, human, yeast, ecoli)
-        target_class (str): class to predict the probability
-                            (secreted (default), cytoplasm, or membrane)
-        include_dg (Boolean): specify inclusion of additional features
-                                (default=False)
-
-    Returns:
-        predicted_class (str): predicted class of sequence
-                        (cytoplasm, membrane, secreted)
-        target_score (float): probability of sequence being 
-                            in target class
-    """
-
-    # target class should be one of the classes of the model
-    if target_class not in ['secreted', 'membrane', 'cytoplasm']:
-        raise ValueError('The selected probability was: "'+str(target_class)+'", but should be either secreted, membrane or cytoplasm')
-    else:
-        pass
-
-    # Select the model based on the organism
-    model = model_organism_selection(organism, include_dg)
-
-    # Obtain unirep representation of sequence
-    # we use the function we created in check_inputs
-    sequence = check_input(input_seq)
-    values = get_UniReps(sequence)[0] # do we need this [0]?
-    if include_dg:
-        dg = calculate_transmembrane_dg(sequence)
-        dg_standardized = (dg - 2.053619) / (2.300432 ** 2)
-        values.append(dg_standardized)
-    values = values.reshape(1, -1)
-
-    # Returns class of given sequence
-    predicted_class = model.predict(values)[0]
-
-    # Grab the probabilities from the model
-    classes = list(model.classes_)
-    prediction_probabilities = list(model.predict_proba(values)[0])
-
-    # Returns probability of being secreted class
-    target_class_score = prediction_probabilities[classes.index(target_class)]
-    
-    return predicted_class, target_class_score
 
 
 def calculate_transmembrane_dg(seq):
@@ -160,3 +110,137 @@ def calculate_transmembrane_dg(seq):
     transmembrane_dG = min(dg_values)
 
     return transmembrane_dG
+
+def predict_loc_simple(input_seq, organism, target_loc, include_dg=False):
+    
+    ## Tests to check inputs
+    # target class should be one of the classes of the model
+    if target_loc not in ['secreted', 'membrane', 'cytoplasm']:
+        raise ValueError('The selected probability was: "'+str(target_loc)+'", but should be either secreted, membrane or cytoplasm')
+    else:
+        pass
+    
+    # Obtain unirep representation of sequence
+    sequence = check_input(input_seq)
+    values = get_UniReps(sequence)[0] # do we need this [0]?
+    if include_dg:
+        dg = calculate_transmembrane_dg(sequence)
+        dg_standardized = (dg - 2.053619) / (2.300432 ** 2)
+        values = np.append(values, dg_standardized)
+    values = values.reshape(1, -1)
+    
+    # Select the model based on the organism
+    model = model_organism_selection(organism, include_dg)
+    # Grab the probabilities from the model
+    classes = list(model.classes_)
+    prediction_probabilities = list(model.predict_proba(values)[0])
+    
+    pred = prediction_probabilities[classes.index(target_loc)]
+
+    return pred
+
+
+
+
+def predict_location(input_seq, organism='all', target_loc='all', include_dg=False, pred_all=False):
+    """
+    Finds the probability of being the target class given the
+    sequence based on model and UniRep representation only
+
+    Args:
+        input_seq (str): sequence entered by the user
+        organism (str):  specific organism of sequence type 
+                         (all, human, yeast, ecoli)
+        target_loc (str): class to predict the probability
+                            (secreted (default), cytoplasm, or membrane)
+        include_dg (Boolean): specify inclusion of additional features
+                                (default=False)
+
+    Returns:
+        predicted_loc (str): predicted localization of the protein
+                        (cytoplasm, membrane, secreted)
+        target_score (float): probability of sequence being 
+                            in target class
+    """
+    
+    ## Tests to check inputs
+    # target class should be one of the classes of the model
+    if target_loc not in ['secreted', 'membrane', 'cytoplasm','all']:
+        raise ValueError('The selected probability was: "'+str(target_loc)+'", but should be either secreted, membrane, cytoplasm or all(default)')
+    else:
+        pass
+    
+    # Keep query
+    query = {'sequence': input_seq,
+             'organism': organism,
+             'location': target_loc,
+             'include_dg': include_dg
+    }
+    
+    
+    # Obtain unirep representation of sequence
+    sequence = check_input(input_seq)
+    values = get_UniReps(sequence)[0] # do we need this [0]?
+    if include_dg:
+        dg = calculate_transmembrane_dg(sequence)
+        dg_standardized = (dg - 2.053619) / (2.300432 ** 2)
+        values.append(dg_standardized)
+    values = values.reshape(1, -1)
+    
+    # process query
+    if organism == 'all' or pred_all:
+        pred_org = ['human', 'yeast', 'ecoli']
+    else:
+        pred_org = [organism]
+
+    pred_results = pd.DataFrame()
+    
+    # make predictions for each organism
+    for org in pred_org:
+        # Select the model based on the organism
+        model = model_organism_selection(org, include_dg)
+        # Grab the probabilities from the model
+        classes = list(model.classes_)
+        prediction_probabilities = list(model.predict_proba(values)[0])
+        pred_results[org] = pd.Series(prediction_probabilities, index=classes)
+    
+    # filter results accordingly
+    if target_loc == 'all':
+        filt_results = pred_results
+    else:
+        filt_results = pred_results.loc[target_loc]
+    
+    if pred_all and organism != 'all':
+        filt_results = filt_results.loc[:,organism]
+    else:
+        pass
+    
+    # get the best prediction
+    if organism == 'all':
+        org_for_pred = 'human'
+    else:
+        org_for_pred = organism
+    
+    predicted_loc = pred_results.index[np.argmax(pred_results.loc[:,org_for_pred])]
+    predicted_prob = np.max(pred_results.loc[:,org_for_pred])
+    
+    if pred_all:
+        all_predictions = pred_results
+    else:
+        all_predictions = 'not computed'
+    
+    class PredLocalization(object):
+        def __init__(self):
+            self.query = query
+            self.result = filt_results
+            self.predicted_loc = predicted_loc
+            self.predicted_prob = predicted_prob
+            self.all_predictions = all_predictions
+            
+        def __call__(self):
+            return filt_results
+
+    return PredLocalization()
+
+
+
