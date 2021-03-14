a60a1396c1199af1ca349bd2637f023b3c7868e6
Joe Henthorn <Josefh1@uw.edu>
2021-03-14 16:03:56 -0700

    Updates, edits, and unit tests for check_inputs

--- lmpm/check_inputs.py
+++ lmpm/check_inputs.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""This module is a input checker for the LMPM module."""
 
 # ## Check Input Sequences
 
 # This notebook is dedicated to the handling and checking user inputs.
-# 
+#
 # The user should only import the function:  seq = __Check_input(seq)__
-# 
+#
 # * This function returns the origninal sequence if no issues are found.
-# 
-# 
+#
+#
 # * The function all checks for a number of issues including empty strings, numeric inputs, short peptides sequences, and bad character inputs (e.g. '$').
-# 
-# 
+#
+#
 # * The function also detects if the amino acid input sequence is in a three letter code format. It also checks if the three letter code is all upper case, lower case, or mixed case, and converts the three letter code into the single letter code representation. The fixed sequence is then returned.
-# 
+#
 
-
-amino_acids = {'A':'Ala','G':'Gly','I':'Ile','L':'Leu','P':'Pro',
-               'V':'Val','F':'Phe','W':'Trp','Y':'Tyr','D':'Asp',
-               'E':'Glu','R':'Arg','H':'His','K':'Lys','S':'Ser',
-               'T':'Thr','C':'Cys','M':'Met','N':'Asn','Q':'Gln',}
 
 
 
 def check_string(seq):
+    """Checks if the seq is a string
+    """
     if not isinstance(seq, str):
-        raise ValueError('Input is not a string.')
+        #raise ValueError('Input is not a string.')
+        assert False, 'Input is not a string.'
     else:
         pass
     return None
 
+
 def check_length(seq):
+    """Checks if if the seq length is too short.
+    """
     if len(seq) < 2: # the shortest protein is TAL and it is 11 AA's long
-        assert False, "The input "+"'"+seq+"'"+" is not an amino acid."
+        #raise ValueError("The input " + "'" + seq + "'" + " is not an amino acid.")
+        assert False, "The input " + "'" + seq + "'" + " is not an amino acid."
     elif len(seq) >=2 and len(seq) < 11: # the shortest protein is TAL and it is 11 AA's long
-        assert False, "The peptide sequence "+"'"+seq+"'"+" is not an amino acid."
+        #raise ValueError("The peptide sequence " + "'" + seq + "'" + " is not an amino acid.")
+        assert False, "The peptide sequence " + "'" + seq + "'" + " is not an amino acid."
     else:
         pass
-    
-    
+
+
 def check_empty(seq):
     """This function checks that the input is not empty or some null value.
     """
@@ -57,20 +61,20 @@ def check_empty(seq):
     else:
         pass
     if issue is True:
-        assert False, ("The input is empty or not a valid input. " 
+        assert False, ("The input is empty or not a valid input. "
                         "Use only the single letter codes of the 20 essential amino acids")
     else:
         pass
-        
-        
-         
+
+
+
 def check_bad_chars(seq, amino_acids):
     """ This checks for bad characters in amino acid string.
     """
     bad_characters = []
     for character in seq:
         if character not in amino_acids.keys():
-            bad_characters = bad_characters+[character]
+            bad_characters = bad_characters + [character]
         else:
             pass
     return bad_characters
@@ -80,49 +84,50 @@ def check_bad_chars(seq, amino_acids):
 # This raises an error if bad characters are found.
 def raise_error_bad_char(bad_characters):
     """ This function just raises an assert if there are bad characters in the input.
-    """
+        Note: This function is always proceeded by check_bad_chars()
+        This function always takes single character outputs from the bad_characters list
+    """ 
     if bad_characters != []:
-        ', '.join(map(str, bad_characters))
         assert False, ("Your amino acid entry contains the following "
-                                      "invalid characters: " + ', '.join(map(str, bad_characters))
-                                      +". Use only the single letter codes of the 20 essential amino acids")
+                        "invalid characters: " + ', '.join(map(str, bad_characters))
+                        + ". Use only the single letter codes of the 20 essential amino acids")
     else:
         pass
 
 
 
-def check_for_three_letter_code(seq):
+def check_for_three_letter_code(seq, amino_acids):
     """This code is checking to see if user entered three letter AA codes.
     """
 
     if not seq.islower() and not seq.isupper(): # Mixed sequence
-        
+
         if len(seq)%3 == 0: # Could be three letter codes
 
-            
+
             AA_code_len = 3
             # Get every three elements in string.
-            AA_list = [seq[x:x+AA_code_len] for x in range(0, len(seq), AA_code_len)] 
-            seq_new = '' 
-            
+            AA_list = [seq[x:x+AA_code_len] for x in range(0, len(seq), AA_code_len)]
+            seq_new = ''
+
             for AA in AA_list:
                 if AA in amino_acids.values():
-                    
+
                     # Convert all three letter AA's to single letter AA's
                     for key, value in amino_acids.items():
                         if AA == value:
                             seq_new += key
-                    
+
                 elif AA not in amino_acids.values():
-                    bad_characters = check_bad_chars(seq,amino_acids)  
+                    bad_characters = check_bad_chars(seq, amino_acids)
                     raise_error_bad_char(bad_characters)
-                    
-            return seq_new 
-        
+
+            return seq_new
+
         else:
-            assert False, ("The input is not a valid input. " 
+            assert False, ("The input is not a valid input. "
                         "Use only the single letter codes of the 20 essential amino acids")
-    
+
     elif seq.isupper(): # Check if three potential letter sequence is all upper case
 
         if len(seq)%3 == 0: # Could be three letter codes
@@ -130,32 +135,32 @@ def check_for_three_letter_code(seq):
             AA_code_len = 3
             AA_list = [seq[x:x+AA_code_len] for x in range(0, len(seq), AA_code_len)]
             seq_new = ''
-            
+
             for AA in AA_list:
                 if AA in {v.upper() for v in amino_acids.values()}: # Checking if input is 3 letter upper case
                     # Convert all three letter AA's to single letter AA's
                     for key, value in amino_acids.items():
                         if AA == value.upper():
                             seq_new += key
-                            
+
                 elif AA not in amino_acids.values():
-                    bad_characters = check_bad_chars(seq,amino_acids)
+                    bad_characters = check_bad_chars(seq, amino_acids)
                     if bad_characters == []:
                         return seq
                         break
                     else:
                         raise_error_bad_char(bad_characters)
-            
-            bad_characters = check_bad_chars(seq_new,amino_acids)  
+
+            bad_characters = check_bad_chars(seq_new, amino_acids)
             raise_error_bad_char(bad_characters)
-            return seq_new  
-        
-        bad_characters = check_bad_chars(seq,amino_acids)  
+            return seq_new
+
+        bad_characters = check_bad_chars(seq, amino_acids)
         raise_error_bad_char(bad_characters)
-        return False       
-            
-    elif seq.islower(): # Check if three potential letter sequence is all lower case 
-#         assert False, ("The input is not a valid input. " 
+        return False
+
+    elif seq.islower(): # Check if three potential letter sequence is all lower case
+#         assert False, ("The input is not a valid input. "
 #                         "Use only the upper case single letter codes of the 20 essential amino acids")
 
         if len(seq)%3 == 0: # Could be three letter codes
@@ -163,45 +168,51 @@ def check_for_three_letter_code(seq):
             AA_code_len = 3
             AA_list = [seq[x:x+AA_code_len] for x in range(0, len(seq), AA_code_len)]
             seq_new = ''
-            
+
             for AA in AA_list:
                 if AA in {v.lower() for v in amino_acids.values()}: # Checking if input is 3 letter lower case
                     # Convert all three letter AA's to single letter AA's
                     for key, value in amino_acids.items():
                         if AA == value.lower():
                             seq_new += key
-                            
+
                 elif AA not in {v.lower() for v in amino_acids.values()}:
                     seq = seq.upper
-                    bad_characters = check_bad_chars(seq,amino_acids)
+                    bad_characters = check_bad_chars(seq, amino_acids)
                     if bad_characters == []:
                         return seq
                         break
                     else:
                         raise_error_bad_char(bad_characters)
-            
-            bad_characters = check_bad_chars(seq_new,amino_acids)  
+
+            bad_characters = check_bad_chars(seq_new, amino_acids)
             raise_error_bad_char(bad_characters)
-            return seq_new  
-        
-        bad_characters = check_bad_chars(seq,amino_acids)  
+            return seq_new
+
+        bad_characters = check_bad_chars(seq, amino_acids)
         raise_error_bad_char(bad_characters)
     else:
-        return False 
-    
+        return False
+
+
 def check_input(seq):
     """ This function is the master function/ wrapper for the check input functions
     """
+    amino_acids = {'A':'Ala','G':'Gly','I':'Ile','L':'Leu','P':'Pro',
+               'V':'Val','F':'Phe','W':'Trp','Y':'Tyr','D':'Asp',
+               'E':'Glu','R':'Arg','H':'His','K':'Lys','S':'Ser',
+               'T':'Thr','C':'Cys','M':'Met','N':'Asn','Q':'Gln',}
+
     check_string(seq)
     check_length(seq)
     check_empty(seq)
 
     output = ''
-    output = check_for_three_letter_code(seq) 
-    output
-    if output != False:
+    output = check_for_three_letter_code(seq, amino_acids)
+    #output
+    if output is not False:
         seq = output
-        bad_characters = check_bad_chars(seq,amino_acids)  
+        bad_characters = check_bad_chars(seq, amino_acids)
         raise_error_bad_char(bad_characters)
     else:
         pass
@@ -210,21 +221,21 @@ def check_input(seq):
 
 
 ### Example of running different cases for expected outcome and errors.
-# # Six cases of different inputs cases.
+# # 11 cases of different inputs cases.
 # seq1 = 'ATGILPQNMCTFWDPSCVAGINMWRTC' # Case 1: Expected good input
 # seq2 = '                     ' # Case 2 empty string
-# seq3 = 'A' # Case 3 single amino acid
-# seq4 = 'AAAAA' # Case 4 amino acid less than 11
-# seq5 = 4 # Case 5 number input
-# seq6 = 'ATGILPQXMCTFWDPSCUAGINMWRTC' # Case 6: Ambigious AA's
-# seq7 = 'ATGIL$PQWMCTFWDPSCUAGINMWRTC' # Case 7: Bad characters in AA string
-# seq8 = 'AlaHisLysThrValPheMetLysProTrpAsnGlnIleGlyArgLysArgCysSer' # Case 8: Three letter AA codes
-# seq9 = 'ALAHISLYSTHRVALPHEMETLYSPROTRPASNGLNILEGLYARGLYSARGCYSSER' # Case 9: Three letter AA codes all upper case
-# seq10 = 'alahislysthrvalphemetlysprotrpasnglnileglyarglysargcysgly' # Case 10: Three letter AA codes all lower case
+# seq3 = 'ATG      LMCS        ' # Case 2 white space string
+# seq4 = 'A' # Case 3 single amino acid
+# seq5 = 'AAAAA' # Case 4 amino acid less than 11
+# seq6 = 4 # Case 5 number input
+# seq7 = 'ATGILPQXMCTFWDPSCUAGINMWRTC' # Case 6: Ambigious AA's
+# seq8 = 'ATGIL$PQWMCTFWDPSCUAGINMWRTC' # Case 7: Bad characters in AA string
+# seq9 = 'AlaHisLysThrValPheMetLysProTrpAsnGlnIleGlyArgLysArgCysSer' # Case 8: Three letter AA codes
+# seq10 = 'ALAHISLYSTHRVALPHEMETLYSPROTRPASNGLNILEGLYARGLYSARGCYSSER' # Case 9: Three letter AA codes all upper case
+# seq11 = 'alahislysthrvalphemetlysprotrpasnglnileglyarglysargcysgly' # Case 10: Three letter AA codes all lower case
 
 
 # seq = seq1
 
 # seq = check_input(seq)
 # print("Sequence = "+seq)
-
--- lmpm/tests/test_check_inputs.py
+++ lmpm/tests/test_check_inputs.py
@@ -1,27 +1,160 @@
 """
 Write tests for the check_inputs.py functions.
+
 """
 
-from lmpm import check_inputs
+import lmpm.check_inputs as check_inputs
+
+# def test_check_string():
+#     # test passing a value that is not a string
+#     try:
+#         check_inputs.check_string(4)
+#     except Exception as exep:
+#         # if it is not, it should raise a ValueError
+#         assert isinstance(exep, ValueError), 'Passing a non-string input did not raise a ValueError.'
+
+#     # always return None in tests
+#     return None
 
 def test_check_string():
     # test passing a value that is not a string
     try:
         check_inputs.check_string(4)
     except Exception as exep:
-        # if it is not, it should raise a ValueError
-        assert isinstance(exep, ValueError), 'Passing a non-string input did not raise a ValueError.'
-
-    # always return None in tests
+        assert isinstance(exep, AssertionError), "An unplanned error occured. Not an 'AssertionError.'"
     return None
+
+
+
+def test_check_length():
+    seq4 = 'A' # Case 3 single amino acid
+    seq5 = 'AAAAA' # Case 4 amino acid less than 11
+    
+    seqences = [seq4, 'AA', 'AAA', seq5, 'AAAAAAAAAA', 'AAAAAAAAAAA']
+
+    for seq in seqences:
+        try:
+            check_inputs.check_length(seq)
+
+        except Exception as exep:
+            assert isinstance(exep, AssertionError), ("An unplanned error occured. Not an 'AssertionError.'"
+                                                        " Error with test: " + str(seq))
+    return None
+
+
+
+def test_check_empty():
+    seq1 = '                     ' # Case 2 empty string
+    seq2 = 'ATG      LMCS        ' # Case 2 white space string
+    seq3 = 'NaN'
+    seq4 = 'NA'
+    seq5 = ''
+    seq6 = []
+
+    seqences = [seq1, seq2, seq3, seq4, seq5, seq6]
+
+    for seq in seqences:
+        try:
+            check_inputs.check_empty(seq)
+
+        except Exception as exep:
+            assert isinstance(exep, AssertionError), ("An unplanned error occured. Not an 'AssertionError.'"
+                                                        " Error with test: " + str(seq))
+    return None
+
+
+
+def test_check_bad_chars():
+    amino_acids = {'A':'Ala','G':'Gly','I':'Ile','L':'Leu','P':'Pro',
+               'V':'Val','F':'Phe','W':'Trp','Y':'Tyr','D':'Asp',
+               'E':'Glu','R':'Arg','H':'His','K':'Lys','S':'Ser',
+               'T':'Thr','C':'Cys','M':'Met','N':'Asn','Q':'Gln',}
+    
+    seq7 = 'ATGILPQXMCTFWDPSCUAGINMWRTC' # Case 6: Ambigious AA's
+    seq8 = 'ATGIL$PQWMCTFWDPSCUAGINMWRTC' # Case 7: Bad characters in AA string
+
+    seqences = [seq7, seq8]
+
+    for seq in seqences:
+        try:
+            check_inputs.check_bad_chars(seq, amino_acids)
+
+        except Exception as exep:
+            assert isinstance(exep, AssertionError), ("An unplanned error occured. Not an 'AssertionError.'"
+                                                        " Error with test: " + str(seq))
+    return None
+
 
 
 def test_raise_error_bad_char():
     # try passing some invalid characters and see if it raises an error
-    try:
-        check_inputs.raise_error_bad_char('ZZ')
-    except Exception as exep:
-        assert isinstance(exep, AssertionError), 'Passing invalid characters did not raise an AssertionError.'
-        print(exep)
+    
+    bad_characters = ['*', '.', '#', '$', 'X', '&', 'U', 'Z']
 
+    try:
+        check_inputs.raise_error_bad_char(bad_characters)
+
+    except Exception as exep:
+        assert isinstance(exep, AssertionError), ("An unplanned error occured. Not an 'AssertionError.'"
+                                                    " Error with test: " + str(bad_characters))
     return None
+
+
+
+def test_check_for_three_letter_code():
+    amino_acids = {'A':'Ala','G':'Gly','I':'Ile','L':'Leu','P':'Pro',
+           'V':'Val','F':'Phe','W':'Trp','Y':'Tyr','D':'Asp',
+           'E':'Glu','R':'Arg','H':'His','K':'Lys','S':'Ser',
+           'T':'Thr','C':'Cys','M':'Met','N':'Asn','Q':'Gln',}
+    
+    seq9 = 'AlaHisLysThrValPheMetLysProTrpAsnGlnIleGlyArgLysArgCysSer' # Case 8: Three letter AA codes
+    seq10 = 'ALAHISLYSTHRVALPHEMETLYSPROTRPASNGLNILEGLYARGLYSARGCYSSER' # Case 9: Three letter AA codes all upper case
+    seq11 = 'alahislysthrvalphemetlysprotrpasnglnileglyarglysargcysgly' # Case 10: Three letter AA codes all lower case
+
+    seqences = [seq9, seq10, seq11]
+
+    for seq in seqences:
+        try:
+            check_inputs.check_for_three_letter_code(seq, amino_acids)
+
+        except Exception as exep:
+            assert isinstance(exep, AssertionError), ("An unplanned error occured. Not an 'AssertionError.'"
+                                                        " Error with test: " + str(seq))
+    return None
+
+
+
+def test_check_input():
+    """Testing them all, because why not.
+    """
+    
+    # 11 cases of different inputs cases.
+    seq1 = 'ATGILPQNMCTFWDPSCVAGINMWRTC' # Case 1: Expected good input
+    seq2 = '                     ' # Case 2 empty string
+    seq3 = 'ATG      LMCS        ' # Case 3 white space string
+    seq4 = 'A' # Case 4 single amino acid
+    seq5 = 'AAAAA' # Case 5 amino acid less than 11
+    seq6 = 4 # Case 6 number input
+    seq7 = 'ATGILPQXMCTFWDPSCUAGINMWRTC' # Case 7: Ambigious AA's
+    seq8 = 'ATGIL$PQWMCTFWDPSCUAGINMWRTC' # Case 8: Bad characters in AA string
+    seq9 = 'AlaHisLysThrValPheMetLysProTrpAsnGlnIleGlyArgLysArgCysSer' # Case 9: Three letter AA codes
+    seq10 = 'ALAHISLYSTHRVALPHEMETLYSPROTRPASNGLNILEGLYARGLYSARGCYSSER' # Case 10: Three letter AA codes all upper case
+    seq11 = 'alahislysthrvalphemetlysprotrpasnglnileglyarglysargcysgly' # Case 11: Three letter AA codes all lower case
+
+    seqences = [seq1, seq2, seq3, seq4, seq5, seq6, seq7, seq8, seq9, seq10, seq11]
+
+    for seq in seqences:
+        try:
+            check_inputs.check_input(seq)
+
+        except Exception as exep:
+            assert isinstance(exep, AssertionError), ("An unplanned error occured. Not an 'AssertionError.'"
+                                                        " Error with test: " + str(seq))
+    return None
+
+
+
+
+
+
+
