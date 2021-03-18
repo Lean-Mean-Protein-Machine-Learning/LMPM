7e4c628be26ea032c44a12ed09a5ba745f45b641
marcexpositg <marcexpositg@users.noreply.github.com>
2021-03-18 02:23:43 +0100

    clarification about heroku errors

--- app/app.py
+++ app/app.py
@@ -11,72 +11,119 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 # append this path so that the lmpm module is found
 # another option would be installing it with pip
-sys.path.append('/')
+sys.path.append("/")
 import lmpm
 import lmpm.unirep as unirep
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def load_seqs():
-    seqs = request.form['seqs']
-    specie = request.form['species']
-    localization = request.form['location']
-    add_feats = request.form.get('add_feat') != None
+    """
+    This function loads the sequences on the form of the home page and
+    generates predictions with the model, passing them to the results page.
+    """
+    seqs = request.form["seqs"]
+    specie = request.form["species"]
+    localization = request.form["location"]
+    add_feats = request.form.get("add_feat") != None
 
     try:
-        prediction = lmpm.predict_location(seqs, specie, localization, add_feats, pred_all = True)
+        prediction = lmpm.predict_location(
+            seqs, specie, localization, add_feats, pred_all=True
+        )
     except Exception as e:
         return render_template("input_error.html", error=e)
 
-    return render_template('results.html', seqs=seqs, pred=prediction, prev_species=specie, prev_loc=localization, prev_add_feats=add_feats, tables=[prediction.all_predictions.to_html(classes='data', header="true")])
+    return render_template(
+        "results.html",
+        seqs=seqs,
+        pred=prediction,
+        prev_species=specie,
+        prev_loc=localization,
+        prev_add_feats=add_feats,
+        tables=[
+            prediction.all_predictions.to_html(classes="data", header="true")
+        ],
+    )
 
 
-@app.route('/improve', methods=['POST'])
+@app.route("/improve", methods=["POST"])
 def mutate():
-    seqs = request.form['seqs']
-    specie = request.form['species']
-    localization = request.form['location']
-    add_feats = request.form.get('add_feat') != None #if it is checeked is not empty so get True
-    positions = request.form['positions']
-    
+    """
+    This function takes the sequences and options from the form in the results
+    page and used them to improve the original sequence.
+    """
+    seqs = request.form["seqs"]
+    specie = request.form["species"]
+    localization = request.form["location"]
+    add_feats = (
+        request.form.get("add_feat") != None
+    )  # if it is checeked is not empty so get True
+    positions = request.form["positions"]
+
     try:
-        mutated_scores, initial_score = lmpm.optimize_sequence(seqs, specie, localization, include_dg=add_feats, positions=positions)
-        plot_f = lmpm.improve_sec.plot_optimization(mutated_scores, initial_score, plot_inplace=False, dpi=300)
-        mut_table = lmpm.top_mutations(mutated_scores, initial_score,15)
-        # help of https://gitlab.com/snippets/1924163 and https://stackoverflow.com/questions/50728328/python-how-to-show-matplotlib-in-flask
+        mutated_scores, initial_score = lmpm.optimize_sequence(
+            seqs,
+            specie,
+            localization,
+            include_dg=add_feats,
+            positions=positions,
+        )
+        plot_f = lmpm.improve_sec.plot_optimization(
+            mutated_scores, initial_score, plot_inplace=False, dpi=300
+        )
+        mut_table = lmpm.top_mutations(mutated_scores, initial_score, 15)
+        # help from https://gitlab.com/snippets/1924163 and
+        # https://stackoverflow.com/questions/50728328/python-how-to-show-
+        # matplotlib-in-flask
         pngImage = io.BytesIO()
         FigureCanvas(plot_f).print_png(pngImage)
         # Encode PNG image to base64 string
-        b64plot = str("data:image/png;base64,")+str(base64.b64encode(pngImage.getvalue()).decode('utf8'))
+        b64plot = str("data:image/png;base64,") + str(
+            base64.b64encode(pngImage.getvalue()).decode("utf8")
+        )
     except Exception as e:
         return render_template("input_error.html", error=e)
 
-    return render_template('improve.html', seqs=seqs, specie=specie, loc=localization, add_feats=add_feats, plot=b64plot, tables=[mut_table.to_html(classes='data', header="true")])
+    return render_template(
+        "improve.html",
+        seqs=seqs,
+        specie=specie,
+        loc=localization,
+        add_feats=add_feats,
+        plot=b64plot,
+        tables=[mut_table.to_html(classes="data", header="true")],
+    )
 
 
-@app.route('/guide')
+@app.route("/guide")
 def get_guide():
-    return render_template('guide.html')
+    return render_template("guide.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def get_about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
-    # use this when running in the website (heroku)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port = port)
-    # this is used if you run it as flask app to debug (Specifying port explicity is important in this case)
+    # use this when running in the website (heroku) or as local web app
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # instead, comment lines above and uncomment below  if you run it as
+    # flask app to debug (specifying port explicity is important for debugging)
     # app.run(debug=True,host='0.0.0.0',port=5000)
     # app.run(debug=True)
